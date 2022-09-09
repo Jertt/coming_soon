5d193debf0bcdf9915b69c23cb325851e492d51a
Jertt <jeartt.th@gmail.com>
2022-09-09 16:17:30 +0200

    responsivity

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,14 +3,13 @@ import { AiOutlineInstagram, AiOutlineFacebook } from 'react-icons/ai'
 
 export default function Footer() {
   return (
-    <div className='fixed w-full px-80 py-7 bottom-0 flex flex-row justify-center z-20  text-white'>
+    <div className='fixed w-full px-4 sm:px-10 lg:px-40 py-7 bottom-0 flex flex-col sm:flex-row justify-center z-20  text-white'>
           
-        <div className="flex items-center mx-4 space-x-2 text-lg font-semibold">
-            <AiOutlineInstagram className='h-6 w-6' /> <a href="https://www.instagram.com/3ambrandcommunication/" target="_blank" rel="noopener noreferrer" className='txt txt-link hover:text-gray-400 after:bg-white'><span>3AM Brand Communication</span></a>
+        <div className="flex items-center mx-auto sm:mx-4 space-x-2 text-lg font-semibold">
+            <AiOutlineInstagram className='h-6 w-6' /> <a href="https://www.instagram.com/3ambrandcommunication/" target="_blank" rel="noopener noreferrer" className='truncate txt txt-link hover:text-gray-400 after:bg-white'><span>3AM Brand Communication</span></a>
         </div>
-          
-        <div className="flex items-center mx-4 space-x-2 text-lg font-semibold">
-            <AiOutlineFacebook className='h-6 w-6' /> <a href="https://www.facebook.com/3ambrandcommunication" target="_blank" rel="noopener noreferrer" className='txt txt-link hover:text-gray-400 after:bg-white'><span>3AM Brand Communication</span></a>
+        <div className="flex items-center mx-auto sm:mx-4 space-x-2 text-lg font-semibold">
+            <AiOutlineFacebook className='h-6 w-6' /> <a href="https://www.facebook.com/3ambrandcommunication" target="_blank" rel="noopener noreferrer" className='truncate txt txt-link hover:text-gray-400 after:bg-white'><span>3AM Brand Communication</span></a>
         </div>
           
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,12 +3,15 @@ import React from 'react'
 export default function Navbar() {
   return (
     <>
-      <nav className="fixed w-full px-80 py-7 top-0 flex flex-row justify-between z-10 bg-black bg-opacity-30 filter backdrop-filter backdrop-blur-lg text-white">
+      <nav className="fixed w-full px-4 sm:px-10 lg:px-40 py-7 top-0 flex flex-row justify-between z-10 bg-black bg-opacity-30 filter backdrop-filter backdrop-blur-lg text-white">
         <div>
             <img src="https://3ambrand.com/assets/img/logo.svg" alt="" />
         </div>
         <div>
-            <a href="tel:123456789" className='txt txt-link hover:text-gray-400 after:bg-white'>+383 12 345 678 </a> <span className='mx-2'>|</span> <a href="tel:123456789"  className='txt txt-link hover:text-gray-400 after:bg-white'>+383 12 345 678</a>
+            <a href="tel:123456789" className='txt txt-link hover:text-gray-400 after:bg-white'>+383 12 345 678 </a>
+            <br className='sm:hidden' />
+             <span className='mx-2 hidden sm:inline'>|</span> 
+            <a href="tel:123456789"  className='txt txt-link hover:text-gray-400 after:bg-white'>+383 12 345 678</a>
         </div>
       </nav>  
     </>
